c28bcdc47e1683531ad81e8103f6cccd15efd24b
Manu Vleurick <manu.vleurick@student.hogent.be>
2022-11-25 00:12:48 +0100

    added support to be able to host app on another hostname. Your first argument on the CLI will be where the app wil run on port 5000. When no arguments it will run on 127.0.0.1

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask,render_template
 import os
+import sys
 
 app = Flask(__name__)
+host = '127.0.0.1'if len(sys.argv) == 1 else sys.argv[1] 
 if os.getenv('FLASK_SECRET_KEY'):
     app.secret_key = os.getenv('FLASK_SECRET_KEY')
 else:
@@ -15,4 +17,4 @@ def home():
 
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(host=host,debug=True)
